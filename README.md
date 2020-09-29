daeabec50dacc67912b84c80754e09cfaf1d8c59
epiccoder2 <epiccoder7@gmail.com>
2020-09-29 10:59:27 -0700

    :sparkles: Add opt-in form component
    Add the opt-in form component for entering your email

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Netflix clone
 
-### A netflix clone made using React, Firebase, Firestore, and Styled Components. 
+### A netflix clone made using React, Firebase, Firestore, and Styled Components.
+
 ---
 
 ## How to setup and use
+
 1. Fork/Clone/Download this repo
 2. Run `yarn` or `npm install` depending on what your using
 3. TODO: Update these steps when the app is complete
 
 ---
 
-## Credit 
+## Credit
+
 Tutorial used for making this is [here](https://www.youtube.com/watch?v=x_EEwGe-a9o&feature=youtu.be). **Full credit goes to him**
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Accordion } from "../components";
+import OptForm from "../components/opt-form";
 import faqsData from "../json/faqs.json";
 
 export function FaqsContainer() {
@@ -13,6 +14,16 @@ export function FaqsContainer() {
                 </Accordion.Item>
             ))}
             <Accordion.Item />
+
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text>
+                    Ready to watch? Enter your email to create or restart your
+                    membership
+                </OptForm.Text>
+            </OptForm>
         </Accordion>
     );
 }
