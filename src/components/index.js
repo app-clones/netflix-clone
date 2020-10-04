cbdbee7386b8b7567af1a6e9a11f648315c364f6
Beatzoid <epiccoder7@gmail.com>
2020-10-04 11:54:49 -0700

    ✨ Add Signin Page
    Add the Signin component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as OptForm } from "./opt-form";
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Feature } from "./feature";
+export { default as Form } from "./form";
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
-    return <p>Hello from signin!</p>;
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const isInvalid = password === "" || emailAddress === "";
+    const handleSignIn = (event) => {
+        event.preventDefault();
+
+        // TODO: Add Firebase here
+    };
+
+    return (
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSignIn} method="post">
+                        <Form.Input
+                            placeholder="Email Address"
+                            value={emailAddress}
+                            onChange={({ target }) =>
+                                setEmailAddress(target.value)
+                            }
+                        />
+                        <Form.Input
+                            type="password"
+                            placeholder="Password"
+                            autoComplete="off"
+                            value={password}
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit">
+                            Sign In
+                        </Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>
+                        New to Netflix?{" "}
+                        <Form.Link to="/signup">Sign up now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure
+                        you're not a bot. Learn more.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
+        </>
+    );
 }
