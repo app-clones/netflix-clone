c13bbb2d2437f8ac58fc0005f820dcbcf7d81280
Beatzoid <epiccoder7@gmail.com>
2020-10-03 18:13:03 -0700

    :sparkles: Add firebase.prod.js
    Add a firebase.prod.js file, which is a template for anyone trying to run this code.

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,25 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// 1) When seeding the database you have to uncomment this!
+// import { seedDatabase } from '../seed';
+
+// 2) See the README for how to get this info
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+// 3) When seeding the database you have to uncomment this as well!
+// 4) Once you have populated the database (***ONLY RUN ONCE***!), re-comment this so you don't get duplicate data.
+// seedDatabase(firebase);
+
+export { firebase };
