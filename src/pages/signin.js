6eb9878c55f7eb94475cc9247af244dcc7fcbffc
Beatzoid <epiccoder7@gmail.com>
2020-10-04 13:29:12 -0700

    ✨ Add Firebase to Signin Page
    Add Firebase Auth to the Signin Page.

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
     const [emailAddress, setEmailAddress] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
     const isInvalid = password === "" || emailAddress === "";
+
     const handleSignIn = (event) => {
         event.preventDefault();
 
-        // TODO: Add Firebase here
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmailAddress("");
+                setPassword("");
+                setError(error.message);
+            });
     };
 
     return (
