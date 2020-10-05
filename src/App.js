499d75da8984d4be10c074d6ab6e19f225b17ce8
Beatzoid <epiccoder7@gmail.com>
2020-10-04 17:27:17 -0700

    ✨ Add Basic Route Protection
    Add basic route protection, to prevent non-logged in users from accessing the browse page, and logged in users from accessing any page other than browse

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Home, Browse, Signin, Signup } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+    // TODO: Add Firebase user here
+    const user = {};
+
     return (
         <Router>
-            <Route exact path={ROUTES.BROWSE}>
-                <Browse />
-            </Route>
-            <Route exact path={ROUTES.SIGN_UP}>
-                <Signup />
-            </Route>
-            <Route exact path={ROUTES.SIGN_IN}>
+            <IsUserRedirect
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                path={ROUTES.SIGN_IN}
+            >
                 <Signin />
-            </Route>
-            <Route exact path={ROUTES.HOME}>
+            </IsUserRedirect>
+
+            <IsUserRedirect
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                path={ROUTES.SIGN_UP}
+            >
+                <Signup />
+            </IsUserRedirect>
+
+            <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+                <Browse />
+            </ProtectedRoute>
+
+            <IsUserRedirect
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                path={ROUTES.HOME}
+                exact
+            >
                 <Home />
-            </Route>
+            </IsUserRedirect>
         </Router>
     );
 }
