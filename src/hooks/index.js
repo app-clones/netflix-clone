769b1783005c9341c8689c7206d81116daf94f84
Beatzoid <epiccoder7@gmail.com>
2020-10-09 16:49:33 -0700

    ✨ Custom Hook for Browse Page
    Add a custom hook for fetching either all series or all films from the database

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,2 @@
+export { default as useContent } from "./use-content";
 export { default as useAuthListener } from "./use-auth-listener";
---
+++ src/hooks/use-content.js
@@ -0,0 +1,28 @@
+import { useEffect, useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+
+export default function useContent(target) {
+    const [content, setContent] = useState([]);
+    const { firebase } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection(target)
+            .get()
+            .then((snapshot) => {
+                const allContent = snapshot.docs.map((contentObj) => ({
+                    ...contentObj.data(),
+                    docId: contentObj.id
+                }));
+
+                setContent(allContent);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+        // eslint-disable-next-line
+    }, []);
+
+    return { [target]: content };
+}
