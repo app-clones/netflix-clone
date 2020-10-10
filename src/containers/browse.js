0fd1c9cbab5fb51b1cf27f8dd0a1169d865914c2
Beatzoid <epiccoder7@gmail.com>
2020-10-09 18:03:42 -0700

    ✨ Add Basic Browse Container
    Add a basic browse container. Later on this will include more things like a profile selector

---
+++ src/containers/browse.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function BrowseContainer({ slides }) {
+    return <p>Hello!</p>;
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { BrowseContainer } from "../containers/browse";
+import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-    return <p>Hello from browse!</p>;
+    const { series } = useContent("series");
+    const { films } = useContent("films");
+    const slides = selectionFilter({ series, films });
+
+    return <BrowseContainer slides={slides} />;
 }
