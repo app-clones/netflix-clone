c4fc2c17895503c109efc9f7c5c0f7880367eedf
Beatzoid <epiccoder7@gmail.com>
2020-10-09 18:21:41 -0700

    ✨ Add Profiles Page
    Add a profiles page, which will not work until I add the profiles component (next commit)

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { SelectProfileContainer } from "../pages/profiles";
 
 export function BrowseContainer({ slides }) {
-    return <p>Hello!</p>;
+    return <SelectProfileContainer />;
 }
---
+++ src/pages/profiles.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
+
+export function SelectProfileContainer({ user, setProfile }) {
+    return (
+        <>
+            <Header bg={false}>
+                <Header.Frame>
+                    <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+                </Header.Frame>
+            </Header>
+
+            <Profiles>
+                <Profiles.Title>Who's watching?</Profiles.Title>
+                <Profiles.List>
+                    <Profiles.User>
+                        <Profiles.Picture src={user.photoURL} />
+                        <Profiles.Name>{user.displayName}</Profiles.Name>
+                    </Profiles.User>
+                </Profiles.List>
+            </Profiles>
+        </>
+    );
+}
