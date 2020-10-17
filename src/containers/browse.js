a01bf2385608cc071b053e5e1d89f6fdddb4ce28
Beatzoid <epiccoder7@gmail.com>
2020-10-16 18:56:25 -0700

    ✨ Add Live Search
    Add live search using Fuse.js

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import React, { useContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
 import { SelectProfileContainer } from "./profiles";
 import { FooterContainer } from "./footer";
 import { FirebaseContext } from "../context/firebase";
@@ -27,6 +28,25 @@ export function BrowseContainer({ slides }) {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [slides, category]);
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ["data.description", "data.title", "data.genre"]
+        });
+
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+
+        if (
+            slideRows.length > 0 &&
+            searchTerm.length > 3 &&
+            results.length > 0
+        ) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+        // eslint-disable-next-line
+    }, [searchTerm]);
 
     return profile.displayName ? (
         <>
