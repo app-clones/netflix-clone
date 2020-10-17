4448bd9f91f630dc6e154f131b179520ca825588
Beatzoid <epiccoder7@gmail.com>
2020-10-17 12:49:29 -0700

    ✅ Add Player Test
    Add the test for the Player component

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -21,7 +21,11 @@ Player.Video = function PlayerVideo({ src, ...restProps }) {
 
     return showPlayer
         ? ReactDOM.createPortal(
-              <Overlay onClick={() => setShowPlayer(false)} {...restProps}>
+              <Overlay
+                  onClick={() => setShowPlayer(false)}
+                  {...restProps}
+                  data-testid="player"
+              >
                   <Inner>
                       <video id="netflix-player" controls>
                           <source src={src} type="video/mp4" />
