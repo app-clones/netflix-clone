44ccd9a9b6c7e7e94a26e1348fb4784c985067bd
Beatzoid <epiccoder7@gmail.com>
2020-10-17 15:26:15 -0700

    ✅ Update Accordion Test
    Update the accordion test to remove a unused import

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, queryByTestId } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import faqsData from "../../json/faqs.json";
 import { Accordion } from "../../components";
 
