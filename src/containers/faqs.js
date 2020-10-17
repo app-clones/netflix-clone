a01f6657025f41c1af63222cf1b200d07db7e241
Beatzoid <epiccoder7@gmail.com>
2020-10-17 15:22:53 -0700

    ✅ Add Accordion Test
    Add the test for the Accordion component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -8,12 +8,11 @@ export function FaqsContainer() {
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
             {faqsData.map((item) => (
-                <Accordion.Item key={item.key}>
+                <Accordion.Item key={item.id}>
                     <Accordion.Header>{item.header}</Accordion.Header>
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
-            <Accordion.Item />
 
             <OptForm>
                 <OptForm.Input placeholder="Email Address" />
