7d5878bff0719716033cc12c9bf252bbbf799d2f
Beatzoid <epiccoder7@gmail.com>
2020-10-17 17:13:28 -0700

    ✅ Add Selection Filter Test
    Add the test for the selection-filter utility

--- src/utils/selection-filter.js
+++ src/utils/selection-filter.js
@@ -1,47 +1,47 @@
-export default function selectionFilter({ series, films }) {
+export default function selectionFilter({ series, films } = []) {
     return {
         series: [
             {
                 title: "Documentaries",
-                data: series.filter((item) => item.genre === "documentaries")
+                data: series?.filter((item) => item.genre === "documentaries")
             },
             {
                 title: "Comedies",
-                data: series.filter((item) => item.genre === "comedies")
+                data: series?.filter((item) => item.genre === "comedies")
             },
             {
                 title: "Children",
-                data: series.filter((item) => item.genre === "children")
+                data: series?.filter((item) => item.genre === "children")
             },
             {
                 title: "Crime",
-                data: series.filter((item) => item.genre === "crime")
+                data: series?.filter((item) => item.genre === "crime")
             },
             {
                 title: "Feel Good",
-                data: series.filter((item) => item.genre === "feel-good")
+                data: series?.filter((item) => item.genre === "feel-good")
             }
         ],
         films: [
             {
                 title: "Drama",
-                data: films.filter((item) => item.genre === "drama")
+                data: films?.filter((item) => item.genre === "drama")
             },
             {
                 title: "Thriller",
-                data: films.filter((item) => item.genre === "thriller")
+                data: films?.filter((item) => item.genre === "thriller")
             },
             {
                 title: "Children",
-                data: films.filter((item) => item.genre === "children")
+                data: films?.filter((item) => item.genre === "children")
             },
             {
                 title: "Suspense",
-                data: films.filter((item) => item.genre === "suspense")
+                data: films?.filter((item) => item.genre === "suspense")
             },
             {
                 title: "Romance",
-                data: films.filter((item) => item.genre === "romance")
+                data: films?.filter((item) => item.genre === "romance")
             }
         ]
     };
