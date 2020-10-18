03ab963df8d65a9712036addac9f49884c3b43ad
Beatzoid <epiccoder7@gmail.com>
2020-10-17 17:09:23 -0700

    ✅ Add Profiles Test
    Add the test for the Profiles container

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -16,6 +16,7 @@ export function SelectProfileContainer({ user, setProfile }) {
                 <Profiles.Title>Who's watching?</Profiles.Title>
                 <Profiles.List>
                     <Profiles.User
+                        data-testid="user-profile"
                         onClick={() =>
                             setProfile({
                                 displayName: user.displayName,
