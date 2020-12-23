f68460d58395e894970d8de7919e882b504412a2
Beatzoid <epiccoder7@gmail.com>
2020-12-23 14:16:17 -0800

    🚨 Update ESLint
    Update the ESLint config to what it was previously

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -23,6 +23,6 @@ OptForm.Text = function OptFormText({ children, ...restProps }) {
     return <Text {...restProps}>{children}</Text>;
 };
 
-OptForm.Break = function OptFormBreak({ _, ...restProps }) {
+OptForm.Break = function OptFormBreak({ children, ...restProps }) {
     return <Break {...restProps} />;
 };
