3eee23c423d7988be13527d83c69e1c7b9bb29ee
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-05-30 10:15:13 -0700

    🚨 Fix ESLint Errors

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Accordion } from "./accordion";
 export { default as Jumbotron } from "./jumbotron";
 export { default as OptForm } from "./opt-form";
 export { default as Footer } from "./footer";
+// eslint-disable-next-line import/no-cycle
 export { default as Header } from "./header";
 export { default as Feature } from "./feature";
 export { default as Form } from "./form";
